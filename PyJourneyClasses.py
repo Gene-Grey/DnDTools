b4425a885c557e2c511ce7eaae79e9b7196add19
Gene-Grey <argentin.joris@gmail.com>
2020-12-28 18:57:28 +0100

    addTime Functionning, coolNames.json created

---
+++ PyJourneyClasses.py
@@ -0,0 +1,171 @@
+"""
+PyJourneyClasses.py
+Sets the classses included into the PyJourney code
+"""
+
+class Group:
+    def __init__(self, members=[]):
+        self.members = members
+
+    def get_members(self):
+        return self.members
+
+    def set_members(self, members):
+        self.members = members
+
+    def add_member(self, member):
+        self.members.append(member)
+
+    def delete_member(self, member_pos):
+        del self.members[member_pos]
+    
+    @property
+    def travelPace(self):
+        speeds = []
+
+        for member in self.members:
+            memberSpeed = convert_fpt_to_mph(member.get_speed())
+            speeds.append(memberSpeed)
+    
+        return min(speeds)
+
+
+class Adventurer:
+    def __init__(self, name="", strength=0, size=0, speed=0, 
+                carryingCapacity = 0, mount=None, passivePerception=0, 
+                metavision=(0, 0), encumberment=1, exhaustionLevel=0):
+        self.name = name
+        self.strength = strength
+        self.size = size
+        self.speed = speed
+        self.mount = mount
+        self.passivePerception = passivePerception
+        self.metavision = metavision
+        self.encumberment = encumberment
+        self.exhaustionLevel = exhaustionLevel
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+    
+    def delete_name(self):
+        self.name = ""
+
+    def get_strength(self):
+        return self.strength
+
+    def set_strength(self, strength):
+        self.strength = strength
+
+    def delete_strength(self):
+        self.strength = 0
+
+    def get_size(self):
+        return self.size
+
+    @property
+    def carryingCapacity(self):
+        return self.strength*self.size*15
+
+    def delete_size(self):
+        self.size = 0
+
+    def get_speed(self):
+        if self.mount is None:
+            return self.speed
+        else:
+            return self.mount.speed
+
+    def set_speed(self, speed):
+        self.speed = speed
+
+    def delete_speed(self):
+        self.speed = 0 
+
+    def get_mount(self):
+        return self.mount
+
+    def set_mount(self, mount):
+        self.mount = mount
+
+    def delete_mount(self):
+        self.mount = None
+    
+    def get_passive_perception(self):
+        return self.passivePerception
+
+    def set_passive_perception(self, passivePerception):
+        self.passivePerception = passivePerception
+
+    def delete_passive_perception(self):
+        self.passivePerception = 0
+
+    def get_metavision(self):
+        return self.metavision
+
+    def set_metavision(self, metavisionTuple):
+        self.metavision = metavisionTuple
+
+    def delete_metavision(self):
+        self.metavision = (0, 0)
+
+    def get_encumberment(self):
+        return self.encumberment
+
+    def set_encumberment(self, encumbermentValue):
+        self.encumberment = encumbermentValue
+
+    def delete_encumberment(self):
+        self.encumberment = 0
+
+class Mount:
+    def __init__(self, name="", strength=0, size=0, speed=0):
+        self.name = name
+        self.strength = strength
+        self.size = size
+        self.speed = speed
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def delete_name(self):
+        self.name = ""
+
+    def get_strength(self):
+        return self.strength
+
+    def set_strength(self, strength):
+        self.strength = strength
+
+    def delete_strength(self):
+        self.strength = 0
+
+    @property
+    def carryingCapacity(self):
+        return self.strength*self.size*15
+
+    def get_size(self):
+        return self.size
+
+    def set_size(self, size):
+        self.size = size
+
+    def delete_size(self):
+        self.size = 0
+
+    def get_speed(self):
+        return self.speed()
+    
+    def set_speed(self, speed):
+        self.speed = speed
+
+    def delete_speed(self):
+        self.speed = 0
+
+def convert_fpt_to_mph(fptSpeed):
+    return fptSpeed/10
--- TimeTracker.py
+++ TimeTracker.py
@@ -1,10 +1,60 @@
-import time
+import warnings
 
-switchOn = True
-dateArray = [3, 1970, 7, 25, 12, 6, 9]
+def addTime(time_adds, dateArray):
+    i = 1
 
-while switchOn == True:
+    while time_adds != []:
+        if type(time_adds[0]) == tuple:
+            print("Turn", i, "start :", dateArray)
+
+            if type(time_adds[0][0]) == int:
+                if type(time_adds[0][1]) == int:
+                    dateArray[time_adds[0][0]] = \
+                    dateArray[time_adds[0][0]] + time_adds[0][1]
+
+                    if dateArray[6] >= 60:
+                        dateArray[5] += dateArray[6] // 60
+                        dateArray[6] = dateArray[6] % 60
+
+                    if dateArray[5] >= 60:
+                        dateArray[4] += dateArray[5] // 60
+                        dateArray[5] = dateArray[5] % 60
+
+                    if dateArray[4] >= 24:
+                        dateArray[3] += dateArray[4] // 24
+                        dateArray[4] = dateArray[4] % 24
+
+                    if dateArray[3] > 28:
+                        dateArray[2] += dateArray[4] // 28
+                        dateArray[3] = dateArray[3] % 28
+
+                    if dateArray[2] > 12:
+                        dateArray[1] += dateArray[2] // 12
+                        dateArray[2] = dateArray[2] % 12
+
+                else:
+                    pass
+                    warnings.warn("Second value in tuple not int, skipped")
+            else:
+                pass
+                warnings.warn("First value in tuple not int, skipped")
+        else:
+            pass
+            warnings.warn("Non tuple element given, skipped")
+
+        del time_adds[0]
+        print("Turn", i, "end :", dateArray, "\n")
+        i += 1
+
+def main():
+    dateArray = [3, 1970, 7, 25, 12, 6, 9]
+    
+    addTimeQueue = []
+    addTime(addTimeQueue, dateArray)
+
     print(dateArray[0], "-", dateArray[1], "/", dateArray[2], "/", \
-        dateArray[3], " ", dateArray[4], ":", f'{dateArray[5]:02}', \
+        dateArray[3], " ", dateArray[4], ":", f'{dateArray[5]:02}',\
         ":", f'{dateArray[6]:02}', sep="")
-    switchOn = False
+
+if __name__ == "__main__":
+    main()
